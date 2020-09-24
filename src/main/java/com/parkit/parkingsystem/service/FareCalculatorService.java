22cb043f10e3770f6acec98f286023779f39f35c
Vanessa Dreux <vanessa.dreux@gmail.com>
2020-09-24 23:47:25 +0200

    Ajout Discount 5%

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,28 +1,37 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.model.Ticket;
+
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
+private boolean isRecurrentUser;
+
+
+    public void calculateFare(Ticket ticket, boolean isRecurrentUser ){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
-
+        TicketDAO ticketDAO = new TicketDAO();
+        this.isRecurrentUser = ticketDAO.getRecurringUsers(ticket.getVehicleRegNumber());
         long inHour = ticket.getInTime().getTime();
         long outHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct --> DONE
         long duration = (outHour - inHour)/60000;
-        if (duration > 30) {
+        //
+        if (duration > 30 )  {
+            Double factor = isRecurrentUser ? (60/0.95) : 60;
+
             switch (ticket.getParkingSpot().getParkingType()) {
                 case CAR: {
-                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR / 60);
+                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR / factor);
                     break;
                 }
                 case BIKE: {
-                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR / 60);
+                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR / factor);
                     break;
                 }
                 default:
@@ -33,5 +42,6 @@ public class FareCalculatorService {
         {
             ticket.setPrice(0);
         }
+
     }
 }
