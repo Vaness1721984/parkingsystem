dd4815579a9079dab3b970caadc6b17a276f6093
Vanessa Dreux <vanessa.dreux@gmail.com>
2020-09-29 22:35:27 +0200

    Ajout commentaires du code

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -6,23 +6,26 @@ import com.parkit.parkingsystem.model.Ticket;
 
 
 public class FareCalculatorService {
-
-private boolean isRecurrentUser;
-
-
-    public void calculateFare(Ticket ticket, boolean isRecurrentUser ){
+//Add boolean isRecurrentUser as parameter to know if user is recurring or not
+public void calculateFare(Ticket ticket, boolean isRecurrentUser ){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
+
+        //Returns if the user is recurring or not
         TicketDAO ticketDAO = new TicketDAO();
-        this.isRecurrentUser = ticketDAO.getRecurringUsers(ticket.getVehicleRegNumber());
+        ticketDAO.getRecurringUsers(ticket.getVehicleRegNumber());
+        //Returns the number of milliseconds associated to field InTime
         long inHour = ticket.getInTime().getTime();
+        //Returns the number of milliseconds associated to field OutTime
         long outHour = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct --> DONE
+        //TODO#1: Some tests are failing here. Need to check if this logic is correct --> DONE
+        //Operation to calculate the duration in minutes
         long duration = (outHour - inHour)/60000;
-        //
+        //If duration is superior to 30 minutes the user will pay a fee
         if (duration > 30 )  {
+            //If the user is recurring a discount of 5% is applied
             Double factor = isRecurrentUser ? (60/0.95) : 60;
 
             switch (ticket.getParkingSpot().getParkingType()) {
@@ -38,6 +41,7 @@ private boolean isRecurrentUser;
                     throw new IllegalArgumentException("Unkown Parking Type");
             }
         }
+        //If duration is less than 30 minutes the user will park for free
         else
         {
             ticket.setPrice(0);
