0cf78a43ee2d3f3678e235e28c909fe8e0374fef
Vanessa Dreux <vanessa.dreux@gmail.com>
2020-10-17 13:44:30 +0200

    Correction bug App

--- src/main/java/com/parkit/parkingsystem/service/InteractiveShell.java
+++ src/main/java/com/parkit/parkingsystem/service/InteractiveShell.java
@@ -10,7 +10,7 @@ public class InteractiveShell {
 
     private static final Logger logger = LogManager.getLogger("InteractiveShell");
 
-    public static void loadInterface(){
+    public static void loadInterface() throws Exception {
         logger.info("App initialized!!!");
         System.out.println("Welcome to Parking System!");
 
